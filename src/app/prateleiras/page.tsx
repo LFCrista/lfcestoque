ad2db4a957815afd9fa59e5038c91ee8a771774f
ReisScode01 <kawareis006@gmail.com>
2025-09-03 09:52:28 -0300

    chore: build ok; corrigir client directives e lints

--- src/app/prateleiras/page.tsx
+++ src/app/prateleiras/page.tsx
@@ -1,5 +1,4 @@
-// 'use client';
-
+"use client";
 import { useState, useEffect, useCallback } from "react";
 import Header from "../../components/Header";
 import { supabase } from "../../lib/supabase";
